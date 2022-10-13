ceebdffbd553a194ef822358602bc8f8b1c49664
Moksleivis <Moksleivis@kitm.local>
2022-10-13 13:44:06 +0300

    your message

---
+++ database/migrations/2022_10_13_070857_create_airports_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAirportsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('airports', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name');
+            $table->string('country_name');
+            $table->decimal('latitude');
+            $table->decimal('longtitude');
+            $table->foreignId('country_id');
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('airports');
+    }
+}
